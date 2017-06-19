d36ae6fc21f38ac00c650578f371926b29e96534
0o0 <0o0@0o0.com>
2017-06-19 16:21:45 +0800

    .

---
+++ MvcZSGC/MvcZSGC/MvcZSGC/Controllers/PUserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcZSGC.Controllers
+{
+    public class PUserController : Controller
+    {
+        // GET: PUser
+        public ActionResult Index()
+        {
+            return View();
+        }
+		public ActionResult Query()
+		{
+			return View();
+		}
+		public ActionResult Add()
+		{
+			return View();
+		}
+	}
+}
